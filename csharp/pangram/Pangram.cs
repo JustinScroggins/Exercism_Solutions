f1772830cf4fc57416683f1777b9e0b651f6f9e2
Justin Scroggins <justinscroggins@outlook.com>
2021-09-27 11:37:24 -0400

    Completed solution

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -5,30 +5,24 @@ public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        // Arrange data
         string alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
         string[] letters = alphabet.Split(',');
         Dictionary<string, bool> dictionaryAlphabet = new Dictionary<string, bool>();
         
         if (!String.IsNullOrEmpty(input.ToLower()))
         {
-            // Iterate through array of alphabet letters
             foreach (string letter in letters)
             {
-                // Add letters to Dictionary
                 dictionaryAlphabet.Add(letter, false);
             }
-            
-            
-            foreach (char letter in input)
+             
+            foreach (char letter in input.ToLower())
             {
-                // Check if dictionary contains each value each iteration through the input 
-                // and if so, setting the value pair for it to false
                 if (dictionaryAlphabet.ContainsKey(letter.ToString()))
-                {
-                    dictionaryAlphabet[letter.ToString()] = true;
+                    {
+                        dictionaryAlphabet[letter.ToString()] = true;
+                    }
                 }
-            }
             
             if (dictionaryAlphabet.ContainsValue(false))
             {
@@ -37,6 +31,5 @@ public static class Pangram
             return true;
         }
         else return false;
-
     }
 }
