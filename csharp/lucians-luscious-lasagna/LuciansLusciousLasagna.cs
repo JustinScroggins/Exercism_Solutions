b12cc36f894514da77590fcec5a9fbc724ca4b8a
Justin Scroggins <justinscroggins@outlook.com>
2021-09-28 22:56:32 -0400

    Initial challenge commit

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,10 @@
+class Lasagna
+{
+    public int ExpectedMinutesInOven() => 40;
+
+    // TODO: define the 'RemainingMinutesInOven()' method
+
+    // TODO: define the 'PreparationTimeInMinutes()' method
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+}
