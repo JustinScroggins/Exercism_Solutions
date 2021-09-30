bba6499ddab5eb924bd0f507f74fad6e5436455a
Justin Scroggins <justinscroggins@outlook.com>
2021-09-30 19:46:22 -0400

    Initial attempt and initla commit of Isogram challenge

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (word.Length < 1)
+        {
+            return true;
+        }
+        Dictionary<string, bool> alphabet = new Dictionary<string, bool>();
+        string alphabetString = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+        string[] formattedAlphabet = alphabetString.Split(",");
+        foreach (string letter in formattedAlphabet)
+        {
+            alphabet.Add(letter, false);
+        }
+        foreach (char letter in word)
+        {
+            if (alphabet.ContainsKey(letter.ToString()))
+            {
+                bool oldVal;
+                alphabet.TryGetValue(letter.ToString(), out oldVal);
+                alphabet[letter.ToString()] = true;
+                return oldVal;
+            }
+        }
+        return false;
+    }
+}
