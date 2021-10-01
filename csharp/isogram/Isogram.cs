2059fceda5ee7351677eda9d05e90e358cff147a
Justin Scroggins <justinscroggins@outlook.com>
2021-10-01 12:40:12 -0400

    Refactored Isogram challenge to use LINQ; solved 12/13 tests

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,35 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        if (word.Length < 1)
-        {
-            return true;
-
-        }
-
-        Dictionary<string, bool> alphabet = new Dictionary<string, bool>();
-        string alphabetString = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-        string[] formattedAlphabet = alphabetString.Split(",");
-        foreach (string letter in formattedAlphabet)
-        {
-            alphabet.Add(letter, false);
-        }
-
-        foreach (char letter in word)
-        {
-            if (alphabet.ContainsKey(letter.ToString()))
-            {
-                bool oldVal;
-                alphabet.TryGetValue(letter.ToString(), out oldVal);
-                alphabet[letter.ToString()] = true;
-                return oldVal;
-            }
-        }
-        return false;
+        word = word.ToLower();
+        string wordFormatted = new string(word.Where(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)).ToArray());
+        wordFormatted = wordFormatted.Trim();
+        var multipleLetters = wordFormatted.GroupBy(x => x)
+            .Where(g => g.Count() > 1)
+            .Select(g => g.Key).ToList();
+        if (multipleLetters.Count > 0) return false;
+        else return true;
     }
 }
