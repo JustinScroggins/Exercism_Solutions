15b897cff3dfd8e463eacf08be0ef6f28f9de81a
Justin Scroggins <justinscroggins@outlook.com>
2021-10-02 23:07:41 -0400

    Refactored string validity check

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,29 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        if (statement.EndsWith('?'))
+        bool HasDigits = statement.All(char.IsDigit);
+        statement = statement.TrimEnd();
+        statement = statement.Trim('\n', '\r', '\t');
+        if (!HasDigits)
         {
-            return "Sure.";
-        }
-        else if (statement == statement.ToUpper())
-        {
-            return "Whoa, chill out!";
-        }
-        else if (statement == statement.ToUpper() && statement.EndsWith('?'))
-        {
-            return "Calm down, I know what I'm doing!";
-        }
-        else if (statement.Contains(""))
-        {
-            return "Fine. Be that way!";
+            if (statement.EndsWith('?'))
+            {
+                return "Sure.";
+            }
+            else if (statement == statement.ToUpper())
+            {
+                return "Whoa, chill out!";
+            }
+            else if (statement == statement.ToUpper() && statement.EndsWith('?'))
+            {
+                return "Calm down, I know what I'm doing!";
+            }
+            else (statement.Contains(""))
+            {
+                return "Fine. Be that way!";
+            }
         }
         else
         {
             return "Whatever.";
         }
+
     }
 }
