15bb8d28ccb13741fcd4f5a24066e776ea89e08b
Justin Scroggins <justinscroggins@outlook.com>
2021-10-04 23:55:04 -0400

    Initial attempt at phone number challenge.

---
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,18 @@
+using System;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+        string formattedNumber = phoneNumber.Trim('-');
+        if (formattedNumber.Contains("555"))
+        {
+            
+        }
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+    {
+        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+    }
+}
