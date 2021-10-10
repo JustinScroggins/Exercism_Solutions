6163568a60d558e113e2c98b590765b554fb4a0d
Justin Scroggins <justinscroggins@outlook.com>
2021-10-09 20:15:03 -0400

    First iteration of challenge.

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -1,28 +1,24 @@
 using System;
+using System.Collections.Generic;
 
-public enum Letters
-{
-    A, E, I, O, U, L, N, R, S, T = 1,
-    D, G = 2,
-    B, C, M, P = 3,
-    F, H, V, W, Y = 4,
-    K = 5,
-    J, X = 8,
-    Q, Z = 10
-}
 public static class ScrabbleScore
 {
+    public static Dictionary<char, int> count = new Dictionary<char, int>();
+    public static string OnePoint { get; } = "a,e,i,o,u,l,n,r,s,t";
+    public static string TwoPoints { get; } = "d,g";
+    public static string ThreePoints { get; } = "b,c,m,p";
+    public static string FourPoints { get; } = "f,h,v,w,y";
+    public static string FivePoints { get; } = "k";
+    public static string EightPoints { get; } = "j,x";
+    public static string TenPoints { get; } = "q,z";
+
     public static int Score(string input)
     {
         int score = 0;
         char[] letters = input.ToCharArray();
         foreach (char letter in letters)
         {
-            bool isMatch = Enum.IsDefined(typeof(Letters), letter.ToString());
-            if (isMatch)
-            {
-                score += (int)Letters[letter];
-            }
+            
         }
         return score;
     }
