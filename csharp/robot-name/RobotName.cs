935d9e9816fb888f8177780ce200af003ef87bed
Justin Scroggins <justinscroggins@outlook.com>
2021-10-10 23:01:46 -0400

    First iteration; completed getter method for Name property

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -6,7 +6,15 @@ public class Robot
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var random = new Random();
+            char letterOne = (char) random.Next(0, chars.Length);
+            char letterTwo = (char) random.Next(0, chars.Length);
+            int min = 001;
+            int max = 999;
+            int number = random.Next(min, max);
+            string name = $"{letterOne}{letterTwo}{number}";
+            return name;
         }
     }
 
