89d1a2625cb4af5d0128b46f90275583d8c46f2c
Justin Scroggins <justinscroggins@outlook.com>
2021-10-10 23:28:16 -0400

    Second iteration at challenge. Switched name getter method to use regex to generate the random name.

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,25 +1,27 @@
 using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 public class Robot
 {
+
+    private string name;
+
     public string Name
     {
         get
         {
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var random = new Random();
-            char letterOne = (char) random.Next(0, chars.Length);
-            char letterTwo = (char) random.Next(0, chars.Length);
-            int min = 001;
-            int max = 999;
-            int number = random.Next(min, max);
-            string name = $"{letterOne}{letterTwo}{number}";
+            Regex regex = new Regex("[0-9]+[a-zA-Z][a-zA-Z][a-zA-Z]", RegexOptions.IgnoreCase);
+            string name = regex.ToString();
             return name;
         }
+        set { name = value; }
     }
+
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Robot robot = new Robot();
+        robot.Name = "";
     }
 }
