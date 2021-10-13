77138b43efaa09833e5c9f58ab2f701781ffc8be
Justin Scroggins <justinscroggins@outlook.com>
2021-10-12 22:25:38 -0400

    Initial attempt at ResistorColor challenge

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public enum Color { Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White }
+    public static int ColorCode(string color)
+    {
+        color = color.ToLower();
+        foreach (char typeName in Enum.GetValues(typeof(Color)))
+        {
+
+        }
+    }
+
+    public static string[] Colors()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
