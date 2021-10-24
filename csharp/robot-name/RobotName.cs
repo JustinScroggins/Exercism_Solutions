0a932b0dd5d07ce318d239ee24201c134584acdb
Justin Scroggins <justinscroggins@outlook.com>
2021-10-24 18:51:38 -0400

    First iteration of RobotName challenge

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,27 +1,36 @@
 using System;
-using System.Reflection;
-using System.Text.RegularExpressions;
+using System.Text;
 
 public class Robot
 {
-
+    Random random = new Random();
     private string name;
-
     public string Name
     {
         get
         {
-            Regex regex = new Regex("[0-9]+[a-zA-Z][a-zA-Z][a-zA-Z]", RegexOptions.IgnoreCase);
-            string name = regex.ToString();
+            int value = random.Next(0, 1000);
+            string name = $"{RandomString()}{value}";
             return name;
         }
-        set { name = value; }
-    }
 
+        private set { }
+    }
 
     public void Reset()
     {
-        Robot robot = new Robot();
-        robot.Name = "";
+        name = "";
+    }
+
+    private string RandomString()
+    {
+        StringBuilder builder = new StringBuilder();
+        char ch;
+        for (int i = 0; i < 3; i++)
+        {
+            ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+            builder.Append(ch);
+        }
+        return builder.ToString();
     }
 }
