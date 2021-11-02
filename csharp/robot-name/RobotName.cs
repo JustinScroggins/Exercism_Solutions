869e558c102489a6e045ce6379795859c2dbe3a2
Justin Scroggins <justinscroggins@outlook.com>
2021-11-01 21:03:32 -0400

    Second iteration of robot challenge; 8 of 10 unit tests passing

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,36 +1,38 @@
 using System;
+using System.Linq;
 using System.Text;
 
 public class Robot
 {
     Random random = new Random();
-    private string name;
+    public Robot()
+    {
+        _name = this.RandomString();
+    }
+
+    private string _name;
+
     public string Name
     {
-        get
-        {
-            int value = random.Next(0, 1000);
-            string name = $"{RandomString()}{value}";
-            return name;
-        }
-
-        private set { }
+        get { return _name; }
+        set { Name = value; }
     }
+
 
     public void Reset()
     {
-        name = "";
+        _name = "";
     }
 
     private string RandomString()
     {
-        StringBuilder builder = new StringBuilder();
-        char ch;
-        for (int i = 0; i < 3; i++)
-        {
-            ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
-            builder.Append(ch);
-        }
-        return builder.ToString();
+        Random rand = new Random();
+        int nums = random.Next(000, 1000);
+        var val = nums.ToString("000");
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string letters = new string(Enumerable.Repeat(chars, 2)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+        string name = $"{letters}{val}";
+        return name;
     }
 }
