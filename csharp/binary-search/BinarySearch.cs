6b3bf62ee270781ea89ae2e67918ec45261c9c30
Justin Scroggins <justinscroggins@outlook.com>
2021-11-02 21:04:32 -0400

    Solved challenge

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        
+        for (int i = 0; i < input.Length; i++)
+        {
+            var binary = (input.Length / 2);
+            if (input[i] == value)
+            {
+                return i;
+                if (i < binary)
+                {
+                    i = binary;
+                }
+            }
+        }
+        return -1;
     }
 }
