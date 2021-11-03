fb201fb04c787d9815c80db151fe40534092576b
Justin Scroggins <justinscroggins@outlook.com>
2021-11-03 10:09:25 -0400

    Initial attempt; solved solution

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string reverse = string.Empty;
+        int length = input.Length - 1;
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reverse += input[i];
+        }
+        return reverse;
+    }
+}
